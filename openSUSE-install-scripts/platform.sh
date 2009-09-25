626b2412147a709b19185e04b820e90da2f5f3c2
M. Edward (Ed) Borasky <znmeb@DreamSong.(none)>
2009-09-25 14:00:07 -0700

    install scripts

---
+++ openSUSE-install-scripts/platform.sh
@@ -0,0 +1,26 @@
+#! /bin/bash -v
+
+# now update to latest software
+sudo zypper modifyrepo --no-refresh --all
+sudo zypper repos -d
+sudo zypper refresh
+sudo zypper update
+
+# utilities
+sudo zypper install -yl command-not-found findutils-locate sudo 
+sudo zypper install -yl bash-completion
+sudo zypper install -yl zip unzip dos2unix unix2dos
+sudo zypper install -yl flash-player
+sudo zypper install -yl nspluginwrapper
+sudo zypper install -yl gvim vim-enhanced vim-data
+sudo zypper install -yl lynx
+sudo zypper install -yl acroread
+sudo zypper install -yl oprofile systemtap blktrace
+sudo zypper install -yl sysstat
+sudo zypper install -yl cpufrequtils
+sudo zypper install -yl zenmap
+sudo zypper install -yl wireshark
+sudo zypper install -yl vsftpd lftp ncftp yast2-ftp-server
+sudo zypper install -yl git git-gui gitk
+
+echo 'Now set up NTP and bootloader, shut down and snapshot.'
