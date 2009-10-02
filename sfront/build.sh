3dcda8d52d13bf914436b3bb3d61f6d6d89f9bc3
M. Edward (Ed) Borasky <znmeb@DreamSong.(none)>
2009-10-02 10:23:01 -0700

    get the path right

--- sfront/build.sh
+++ sfront/build.sh
@@ -5,6 +5,13 @@ wget http://www.cs.berkeley.edu/~lazzaro/sa/sfront.tar.gz
 tar xf sfront.tar.gz
 cd sfront/src
 make
-sudo ln -sf /home/znmeb/Projects/when-harry-met-iannis/sfront/sfront/bin \
+cd ../..
+sudo ln -sf \
+  /home/znmeb/Projects/when-harry-met-iannis/sfront/sfront/bin/sfront \
   /usr/local/bin/
 which sfront
+
+# make examples
+cd sfront/examples
+make
+cd ../..
