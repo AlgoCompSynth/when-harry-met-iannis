95448185fc49828f0af621ba5695d7ddf56148cb
M. Edward (Ed) Borasky <znmeb@DreamScape.(none)>
2009-10-01 23:54:29 -0700

    more tweaks

--- openSUSE-install-scripts/gem-deps.sh
+++ openSUSE-install-scripts/gem-deps.sh
@@ -3,36 +3,42 @@ sudo zypper install ImageMagick ImageMagick-devel ImageMagick-extra
 sudo zypper install libquicktime libquicktime-devel libquicktime-tools
 sudo zypper install libmpeg2-0 libmpeg2-devel
 sudo zypper install libmpeg3-1 libmpeg3-devel libmpeg3-utils
-sudo zypper install gmerlin \
-  gmerlin-OSD \
-  gmerlin-alsa \
-  gmerlin-alsamixer \
-  gmerlin-audiofilters \
-  gmerlin-camelot \
-  gmerlin-cdaudio \
-  gmerlin-edl \
-  gmerlin-effectv \
-  gmerlin-encoders \
-  gmerlin-esd \
-  gmerlin-jpeg \
-  gmerlin-kbd \
-  gmerlin-lqt \
-  gmerlin-mikmod \
-  gmerlin-oss \
-  gmerlin-player \
-  gmerlin-png \
-  gmerlin-postprocess \
-  gmerlin-pulseaudio \
-  gmerlin-subtitles \
-  gmerlin-tiff \
-  gmerlin-transcoder \
-  gmerlin-utils \
-  gmerlin-v4l \
-  gmerlin-videofilters \
-  gmerlin-visualizer \
-  gmerlin-x11 \
-  libgmerlin-devel \
-  libgmerlin-gtk-devel \
-  libgmerlin-gtk0 \
-  libgmerlin0 
-sudo zyppper install ffmpeg
+sudo zypper install gmerlin
+sudo zypper install gmerlin-OSD
+sudo zypper install gmerlin-alsa
+sudo zypper install gmerlin-alsamixer
+sudo zypper install gmerlin-audiofilters
+sudo zypper install gmerlin-camelot
+sudo zypper install gmerlin-cdaudio
+sudo zypper install gmerlin-edl
+sudo zypper install gmerlin-effectv
+sudo zypper install gmerlin-encoders
+sudo zypper install gmerlin-esd
+sudo zypper install gmerlin-jpeg
+sudo zypper install gmerlin-kbd
+sudo zypper install gmerlin-lqt
+sudo zypper install gmerlin-mikmod
+sudo zypper install gmerlin-oss
+sudo zypper install gmerlin-player
+sudo zypper install gmerlin-png
+sudo zypper install gmerlin-postprocess
+sudo zypper install gmerlin-pulseaudio
+sudo zypper install gmerlin-subtitles
+sudo zypper install gmerlin-tiff
+sudo zypper install gmerlin-transcoder
+sudo zypper install gmerlin-utils
+sudo zypper install gmerlin-v4l
+sudo zypper install gmerlin-videofilters
+sudo zypper install gmerlin-visualizer
+sudo zypper install gmerlin-x11
+sudo zypper install libgmerlin-devel
+sudo zypper install libgmerlin-gtk-devel
+sudo zypper install libgmerlin-gtk0
+sudo zypper install libgmerlin0 
+sudo zypper install ffmpeg
+sudo zypper install gmerlin-avdecoder
+sudo zypper install gmerlin-avdecoder-plugins
+sudo zypper install gmerlin-effectv
+sudo zypper install gmerlin-encoders
+sudo zypper install libgmerlin-avdecoder-devel
+sudo zypper install libgmerlin-avdecoder1
--- openSUSE-install-scripts/pd-gem.sh
+++ openSUSE-install-scripts/pd-gem.sh
@@ -1,8 +1,8 @@
 #! /bin/bash -v
 
 # pd
+rm -fr pd-0.42-5*
 wget http://crca.ucsd.edu/~msp/Software/pd-0.42-5.src.tar.gz
-rm -fr pd-0.42-5
 tar xf pd*gz
 cd pd-0.42-5/src
 ./configure
@@ -12,8 +12,8 @@ sudo /sbin/ldconfig
 cd ../..
 
 # gem
+rm -fr gem-0.92-1*
 wget http://gem.iem.at/releases/0.92.1/gem-0.92-1.tar.gz
-rm -fr gem-0.92-1
 tar xf gem*gz
 cd gem-0.92-1/src
 ./autogen.sh
