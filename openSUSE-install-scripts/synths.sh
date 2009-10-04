c3dee8c5566db8adeb11a9cb5fc10b235265ea29
M. Edward (Ed) Borasky <znmeb@DreamSong.(none)>
2009-10-04 02:26:48 -0700

    ask before install

--- openSUSE-install-scripts/synths.sh
+++ openSUSE-install-scripts/synths.sh
@@ -1,13 +1,13 @@
 #! /bin/bash -v
-sudo zypper install -yl cabel
-sudo zypper install -yl cabel-xBasic
-sudo zypper install -yl cecilia
-sudo zypper install -yl csound
-sudo zypper install -yl csound-gui
-sudo zypper install -yl csound-manual
-sudo zypper install -yl csound-tutorial
-sudo zypper install -yl xenharmonics
-sudo zypper install -yl supercollider
-sudo zypper install -yl amSynth
-sudo zypper install -yl ZynAddSubFX
-sudo zypper install -yl beast
+sudo zypper install cabel
+sudo zypper install cabel-xBasic
+sudo zypper install cecilia
+sudo zypper install csound
+sudo zypper install csound-gui
+sudo zypper install csound-manual
+sudo zypper install csound-tutorial
+sudo zypper install xenharmonics
+sudo zypper install supercollider
+sudo zypper install amSynth
+sudo zypper install ZynAddSubFX
+sudo zypper install beast
