cd4305b173d1639727d642bb8a0c704c2f09d9fe
M. Edward (Ed) Borasky <zznmeb@gmail.com>
2010-03-13 22:12:13 -0800

    uploading from October

--- openSUSE-install-scripts/sfront.sh
+++ openSUSE-install-scripts/sfront.sh
@@ -1,18 +1,16 @@
 #! /bin/bash -v
-zypper repos -d
-sudo zypper refresh
-sudo zypper update
-sudo zypper install -t pattern devel_basis
-sudo zypper install -yl sox
+#zypper repos -d
+#sudo zypper refresh
+#sudo zypper update
+#sudo zypper install -t pattern devel_basis
+#sudo zypper install -yl sox
 rm -fr sfront sfront.tar.gz
 wget http://www.cs.berkeley.edu/~lazzaro/sa/sfront.tar.gz
 tar xf sfront.tar.gz
 cd sfront/src
 make
 cd ../..
-sudo ln -sf \
-  /home/znmeb/Projects/when-harry-met-iannis/openSUSE-install-scripts/sfront/bin/sfront \
-  /usr/local/bin/
+cp sfront/bin/sfront /usr/local/bin/
 which sfront
 
 # make examples
